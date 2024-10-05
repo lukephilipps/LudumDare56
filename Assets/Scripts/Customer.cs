cdb8342020e8aecac8251d4eab690f6a8a410be9
Orion Dobos <oriondobos@gmail.com>
2024-10-05 12:16:52 -0700

    navmesh for little guy! and i added logan's UI :P

--- Assets/Scripts/Customer.cs
+++ Assets/Scripts/Customer.cs
@@ -1,28 +1,59 @@
 using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UI;
+
 public enum Satisfaction
 {
     HAPPY,
-    SATISFIED,
+    NEUTRAL,
     UNHAPPY,
     ANGY
+}
+
+public enum OrderState
+{
+    WAITING,
+    ORDERED,
+    DONE
 }
 
 public class Customer : MonoBehaviour
 {
     public Satisfaction currentSatisfaction;
+    public OrderState currentState;
 
-    public Transform emotionTransform;
-    public Transform orderTransform;
+    public Image emotionImage;
+    public Image orderImage;
+
+    private NavMeshAgent agent;
+
+    public Transform[] destination;
 
     void Start()
     {
         currentSatisfaction = Satisfaction.HAPPY;
+        currentState = OrderState.WAITING;
+
+        agent = GetComponent<NavMeshAgent>();
+        agent.SetDestination(destination[0].position);
     }
     
     public void PlaceOrder()
     {
-        Debug.Log("Placing Order...");
-        
-        
+        if (currentState == OrderState.WAITING)
+        {
+            Debug.Log("Placing Order...");
+
+            int order = Random.Range(0, GameManager.Singleton.items.Length);
+            
+            orderImage.sprite = GameManager.Singleton.GetItemSprite(order);
+            orderImage.color = new Color(255, 255, 255, 255);
+
+            emotionImage.sprite = GameManager.Singleton.GetEmotionSprite(Satisfaction.HAPPY);
+            emotionImage.color = new Color(255, 255, 255, 255);
+
+            currentState = OrderState.ORDERED;
+            agent.SetDestination(destination[1].position);
+        }
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
@@ -5,9 +7,36 @@ public class GameManager : MonoBehaviour
     public static GameManager Singleton;
     
     public Sprite[] items;
+    public Sprite[] emotions;
 
-    public Sprite GetSprite(int id)
+    private void Start()
+    {
+        Singleton = this;
+    }
+
+    public Sprite GetItemSprite(int id)
     {
         return items[id];
+    }
+
+    public Sprite GetEmotionSprite(Satisfaction emotion)
+    {
+        switch (emotion)
+        {
+            case Satisfaction.HAPPY:
+                return emotions[0];
+                break;
+            case Satisfaction.NEUTRAL:
+                return emotions[1];
+                break;
+            case Satisfaction.UNHAPPY:
+                return emotions[2];
+                break;
+            case Satisfaction.ANGY:
+                return emotions[3];
+                break;
+        }
+
+        return null;
     }
 }
